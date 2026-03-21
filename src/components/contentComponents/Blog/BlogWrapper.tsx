e76dd74a89431bedb6282abf67521db0593b9e5b
JurijsRafalskis <bobrfromhell@gmail.com>
2026-03-22 00:16:45 +0200

    Typo fix.

--- src/components/contentComponents/Blog/BlogWrapper.tsx
+++ src/components/contentComponents/Blog/BlogWrapper.tsx
@@ -4,7 +4,7 @@ import type { IBlogData } from "./models/IBlogData";
 export interface BlogProps extends IBlogData {}
 
 export const BlogWrapper = (props:BlogProps) => {
-    useTitleHook(`Pixels2Physics - ${props.title}`)
+    useTitleHook(`Pixels2Physics - ${props.title}`);
 
     return <>
         {props.page}
